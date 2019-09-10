758c4be38767558bf524a2efa84ab7c148a04c3a
Allie <adeabreu@udel.edu>
2019-09-10 16:13:10 -0400

    initial commit

---
+++ Lab 2220/Lab2TOC.cpp
@@ -0,0 +1,33 @@
+/*
+ * Lab2TOC.cpp
+ *
+ *  Created on: Sep 3, 2019
+ *      Author: Allie
+ */
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+int main(){
+	/*int a=14;
+	cout<<a<<endl;
+	cout<<&a<<endl;
+	//Must use address to find the specific variable  name
+	int b=4;
+	int *c=&b;
+	cout<<b<<endl;
+	cout<<c<<endl;
+	cout<<&b<<endl;
+	cout<<&c<<endl;
+	*c=*c**c;
+	cout<<b<<endl;*/
+
+
+
+}//main
+
+
+
---
+++ Lab 2220/lab2b.cpp
@@ -0,0 +1,14 @@
+/*
+ * lab2.cpp
+ *
+ *  Created on: Sep 3, 2019
+ *      Author: Allie DeAbreu and Alex Clark
+ */
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+
+
