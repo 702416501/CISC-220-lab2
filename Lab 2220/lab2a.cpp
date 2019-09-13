1f902fd0f0e1180667a4cf6976fde5820b598ed3
Allie <adeabreu@udel.edu>
2019-09-12 21:33:57 -0400

    problem 6

--- Lab 2220/lab2a.cpp
+++ Lab 2220/lab2a.cpp
@@ -3,6 +3,8 @@
  *
  *  Created on: Sep 3, 2019
  *      Author: Allie DeAbreu and  Alex Clark
+ *
+ *      Function numbers correspond to the problem numbers
  */
 #include <iostream>
 #include <stdlib.h>
@@ -18,24 +20,86 @@ void func1(){
 
 void func2(int b){
 	b+=4;
-	cout<<b<<endl;
-	cout<<&b<<endl;
+	cout<<b<<"\t"<<&b<<endl;
 	return;
 }//need to add the variable to the main function; see second part of problem 2
 
 int func3(){
 	int c = 37;
-	cout<<c<<endl;
-	cout<<&c<<endl;
+	cout<<c<<"\t"<<&c<<endl;
 	return c;
-}//p3; Need to complete the parts for the main function
+}//p3
 
-void func4(int &d){
-	int *y=&d;
-	*y = (*y)*(*y)*(*y);
-
+void func4(int *d){
+	*d = *d * *d * *d;
+	cout<<*d<<"\t"<<d<<"\t"<<&d<<endl;
+	return;
 }
+
+void func5(int &e){
+	int x = rand() % 10 + 1;
+	e = e + x;
+	cout<<x<<"\t"<<e<<"\t"<<&e<<endl;
+	return;
+}
+
+void func6(int *f, int *g){
+	int *h = f;
+	*h = 32;
+	h = g;
+	*h = 42;
+	return;
+}
+
  int main(){
+	 //p1
+	 cout<<"Problem 1"<<endl;
+	 func1();
+	 cout<<"\n"<<endl;//puts 2 lines between this and the next question
+
+	 //p2
+	 cout<<"Problem 2"<<endl;
+	 int aa = 4;
+	 cout<<aa<<"\t"<<&aa<<endl;
+	 func2(aa);
+	 cout<<"\n"<<endl;//puts 2 lines between this and the next question
+
+	 //p3
+	 cout<<"Problem 3"<<endl;
+	 int bb = func3();
+	 cout<<bb<<"\t"<<&bb<<endl;
+	 cout<<"\n"<<endl;
+
+	 //p4
+	 cout<<"Problem 4"<<endl;
+	 int cc = 4;
+	 cout<<cc<<"\t"<<&cc<<endl;
+	 func4(&cc);
+	 cout<<cc<<"\t"<<&cc<<endl;
+	 cout<<"\n"<<endl;
+
+
+	 //p5
+	 cout<<"Problem 5"<<endl;
+	 int dd = 4;
+	 cout<<dd<<"\t"<<&dd<<endl;
+	 func5(dd);
+	 cout<<dd<<"\t"<<&dd<<endl;
+	 cout<<"\n"<<endl;
+
+
+	 //p6
+	 cout<<"Problem 6"<<endl;
+	 int ee = 10;
+	 int ff = 20;
+	 func6(&ee, &ff);
+	 cout<<ee<<"\t"<<ff<<endl;
+	 cout<<"\n"<<endl;
+
+	 //p7
+	 cout<<"Problem 7"<<endl;
+
+
 
 
 	 return 0;
